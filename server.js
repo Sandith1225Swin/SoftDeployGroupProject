ea55fe02e15ec005594dcf1df7b14953f8af8bd6
Sandith Laksiha Gunawickrema <167272917+Sandith1225Swin@users.noreply.github.com>
2024-10-16 20:59:50 +1100

    commit the new

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${3000}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${3000}`);
 });
+
